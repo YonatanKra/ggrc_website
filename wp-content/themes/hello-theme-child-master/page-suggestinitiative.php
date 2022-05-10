b818c9e79cfc7af08143ab4dd169709e3ea18a4a
philipmignon <phillip_huynh@outlook.com>
2022-05-10 00:38:45 -0700

    Adding new theme

---
+++ wp-content/themes/hello-theme-child-master/page-suggestinitiative.php
@@ -0,0 +1,66 @@
+<?php
+
+/* Template Name: SuggestInitiative */ 
+
+get_header("suggest-initiative");
+?>
+
+
+				
+
+	<?php do_action( 'ocean_before_content_wrap' ); ?>
+
+	<div id="content-wrap" class="container clr">
+
+		<?php do_action( 'ocean_before_primary' ); ?>
+
+		<div id="primary" class="content-area clr no-border">
+			<div >
+					<div class="row suggest-titlearea">
+						<div class="col-lg-12 col-md-12 col-sm-12">
+							<h2>Suggest an Initiative</h2>
+							<p><b>Do you want to share your program, project, or campaign with the GGRC community? Suggest your initiative to our team! Read our <a href="<?php echo site_url('wp-content/uploads/ggrc_documents/GGRCInitiativeGuidelines.pdf') ?>" class="text-primary" target="_blank">Initiative Guidelines</a> first.</b></p>
+						</div>
+						
+					</div>
+				
+				</div>
+
+			<?php do_action( 'ocean_before_content' ); ?>
+		
+			<div id="content" class="site-content clr initiative-form">
+
+				<?php do_action( 'ocean_before_content_inner' ); ?>
+
+				<?php
+				// Elementor `single` location.
+				if ( ! function_exists( 'elementor_theme_do_location' ) || ! elementor_theme_do_location( 'single' ) ) {
+
+					// Start loop.
+					while ( have_posts() ) :
+						the_post();
+
+						get_template_part( 'partials/page/layout' );
+
+					endwhile;
+
+				}
+				?>
+
+				<?php do_action( 'ocean_after_content_inner' ); ?>
+
+			</div><!-- #content -->
+
+			<?php do_action( 'ocean_after_content' ); ?>
+
+		</div><!-- #primary -->
+
+		<?php do_action( 'ocean_after_primary' ); ?>
+
+	</div><!-- #content-wrap -->
+
+	<?php do_action( 'ocean_after_content_wrap' ); ?>
+
+	
+
+<?php get_footer(); ?>
